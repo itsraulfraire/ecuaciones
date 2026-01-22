71c5880ae6876536ed60ec411cb86b5004ccba05
itsraulfraire <rfrairegaytan@gmail.com>
2026-01-21 23:20:10 -0600

    Add index route and keep ecuaciones route

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return render_template("ecuaciones.html")
+
+@app.route("/ecuaciones")
 def ecuaciones():
     return render_template("ecuaciones.html")
 
